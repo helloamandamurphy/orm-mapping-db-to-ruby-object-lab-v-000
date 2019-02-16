32aa1922e7256ff299bfa139e7f88357a481c796
Amanda Murphy <helloamandamurphy@gmail.com>
2019-02-16 21:32:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -71,6 +71,11 @@ class Student
   end
 
   def self.all_students_in_grade_x(x)
+    sql = "SELECT * FROM students WHERE grade = ?"
+    
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def save
